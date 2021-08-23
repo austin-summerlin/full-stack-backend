f07a3bfcee343323f2662ad63cadda2a9f14a6dc
Austin Summerlin <summerlin.austin@gmail.com>
2021-08-23 14:42:21 -0700

    spelling

--- data/movies.js
+++ data/movies.js
@@ -36,7 +36,7 @@ const movies = [
   },
 
   {
-    title: 'A Girl Walks Home Alone At Night',
+    title: 'A Girl Walks Home Alone at Night',
     genre: 'Vampire',
     year: 2014,
     director: 'Ana Lily Amirpour',
@@ -64,12 +64,117 @@ const movies = [
 
   {
     title: 'Cannibal Holocaust',
-    genre: 'Giallo',
+    genre: 'Giallo, Cannibal',
     year: 1980,
     director: 'Ruggero Deodato',
     country: 'Italy',
     image: '/movies/cannibal.png'
-  }
+  },
+  {
+    title: 'Scream',
+    genre: 'Slasher',
+    year: 1996,
+    director: 'Wes Craven',
+    country: 'US',
+    image: '/movies/scream.png'
+  },
+  {
+    title: 'The Exorcist',
+    genre: 'Exorcism',
+    year: 1973,
+    director: 'William Friedkin',
+    country: 'US',
+    image: '/movies/exorcist.png'
+  },
+  {
+    title: 'Halloween',
+    genre: 'Slasher',
+    year: 1978,
+    director: 'John Carpenter',
+    country: 'US',
+    image: '/movies/halloween.png'
+  },
+  {
+    title: 'Carrie',
+    genre: 'Paranormal, Revenge',
+    year: '1976',
+    director: 'Brian De Palma',
+    country: 'US',
+    image: '/movies/carrie.png'
+  },
+  {
+    title: 'The Texas Chainsaw Massacre',
+    genre: 'Slasher, Cannibal',
+    year: '1974',
+    director: 'Tobe Hooper',
+    country: 'US',
+    image: '/movies/texas.png'
+  },
+  {
+    title: 'zombi 2',
+    genre: 'Zombie, Giallo',
+    year: '1979',
+    director: 'Lucio Fulci',
+    country: 'Italy',
+    image: '/movies/zombi2.png'
+  },
+  {
+    title: 'Night of The Living Dead',
+    genre: 'Zombie',
+    year: '1968',
+    director: 'George A. Romero',
+    country: 'US',
+    image: '/movies/night.png'
+  },
+  {
+    title: 'Day of The Dead',
+    genre: 'Zombie',
+    year: '1985',
+    director: 'George A. Romero',
+    country: 'US',
+    image: '/movies/day.png'
+  },
+  {
+    title: 'Nosferatu',
+    genre: 'Vampire, Silent',
+    year: '1922',
+    director: 'F.W. Murnau',
+    country: 'Germany',
+    image: '/movies/nosferatu.png'
+  },
+  {
+    title: 'Return of The Living Dead',
+    genre: 'Zombie',
+    year: '1985',
+    director: 'Dan O Bannon',
+    country: 'US',
+    image: '/movies/return.png'
+  },
+  {
+    title: 'The Fly',
+    genre: 'Sci-Fi',
+    year: '1986',
+    director: 'David Cronenberg',
+    country: 'US',
+    image: '/movies/fly.png'
+  },
+  {
+    title: 'The Witch',
+    genre: 'Witches, Supernatural',
+    year: '2015',
+    director: 'Robert Eggers',
+    country: 'US',
+    image: '/movies/witch.png'
+  },
+  {
+    title: 'Midsommar',
+    genre: 'Cults',
+    year: '2019',
+    director: 'Ari Aster',
+    country: 'US',
+    image: '/movies/midsommar.png'
+  },
+
 ];
 
 export default movies;
